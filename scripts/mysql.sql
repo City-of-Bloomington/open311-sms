787a0c5aaf56712bfcc3fead9b562ce9787107e1
Abhiroop Bhatnagar <bhatnagarabhiroop@gmail.com>
2013-07-22 14:29:57 +0530

    added people table

--- scripts/mysql.sql
+++ scripts/mysql.sql
@@ -1,6 +1,6 @@
 -- @copyright 2006-2013 City of Bloomington, Indiana
 -- @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.txt
--- @author Abhiroop Bhatnagar <bhatnagarabhiroop@gmail.com>
+-- @authors Abhiroop Bhatnagar <bhatnagarabhiroop@gmail.com>, Cliff Ingham <inghamn@bloomington.in.gov>
 
 CREATE TABLE `query_record` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -11,3 +11,20 @@ CREATE TABLE `query_record` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `phone_number` (`phone_number`)
 ) 
+create table people (
+	id                   int          unsigned not null primary key auto_increment,
+	firstname            varchar(128),
+	middlename           varchar(128),
+	lastname             varchar(128),
+	email                varchar(255),
+	organization         varchar(128),
+	address              varchar(128),
+	city                 varchar(128),
+	state                varchar(128),
+	zip                  varchar(20),
+	department_id        int          unsigned,
+	username             varchar(40)  unique,
+	password             varchar(40),
+	authenticationMethod varchar(40),
+	role varchar
+);
