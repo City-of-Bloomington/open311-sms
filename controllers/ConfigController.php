900572b91cb311c76eedee9f1b1c7c1eef4d3aee
Abhiroop Bhatnagar <bhatnagarabhiroop@gmail.com>
2013-07-22 23:04:32 +0530

    Modified mysql script

---
+++ controllers/ConfigController.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @copyright 2013 City of Bloomington, Indiana
+ * @license http://www.gnu.org/licenses/agpl.txt GNU/AGPL, see LICENSE.txt
+ * @author Abhiroop Bhatnagar <bhatnagarabhiroop@gmail.com>
+ */
+class ConfigController
+{
+	public function index()
+	{
+		
+	}
+}
+?>
--- scripts/mysql.sql
+++ scripts/mysql.sql
@@ -2,6 +2,41 @@
 -- @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.txt
 -- @authors Abhiroop Bhatnagar <bhatnagarabhiroop@gmail.com>, Cliff Ingham <inghamn@bloomington.in.gov>
 
+CREATE TABLE `configuration` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `configName` varchar(255) NOT NULL,
+  `configValue` text,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `configName` (`configName`)
+);
+
+INSERT INTO `configuration` VALUES 
+(1,'Service Discovery URL','https://api.city.gov/discovery.xml'),
+(2,'Open 311 API Key','open311_api_key'),
+(3,'Open311 Jurisdiction Id','api.city.gov'),
+(4,'Get Service List Response','GROUPS'),
+(5,'SMS Character Limit','160'),
+(6,'SMS Keyword','keyword'),
+(7,'SMS Response Format','html'),
+(8,'SMS Body Parameter','Body'),
+(9,'SMS From Parameter','From'),
+(10,'SMS API Key Parameter','api_key_param'),
+(11,'SMS API Key','api_key');
+
+
+CREATE TABLE `people` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `firstname` varchar(128) NOT NULL,
+  `lastname` varchar(128) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `username` varchar(40) DEFAULT NULL,
+  `password` varchar(40) DEFAULT NULL,
+  `authenticationMethod` varchar(40) DEFAULT NULL,
+  `role` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ;
+
 CREATE TABLE `query_record` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `phone_number` varchar(20) NOT NULL,
@@ -10,21 +45,4 @@ CREATE TABLE `query_record` (
   `additional_info` text,
   PRIMARY KEY (`id`),
   UNIQUE KEY `phone_number` (`phone_number`)
-) 
-create table people (
-	id                   int          unsigned not null primary key auto_increment,
-	firstname            varchar(128),
-	middlename           varchar(128),
-	lastname             varchar(128),
-	email                varchar(255),
-	organization         varchar(128),
-	address              varchar(128),
-	city                 varchar(128),
-	state                varchar(128),
-	zip                  varchar(20),
-	department_id        int          unsigned,
-	username             varchar(40)  unique,
-	password             varchar(40),
-	authenticationMethod varchar(40),
-	role varchar
 );
