6d44dc77dcbbd48c31952466973cb74e325376dc
Abhiroop Bhatnagar <bhatnagarabhiroop@gmail.com>
2013-07-30 11:48:04 +0530

    Added Template and Configuration Files

---
+++ controllers/ConfigurationController.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @copyright 2013 City of Bloomington, Indiana
+ * @license http://www.gnu.org/licenses/agpl.txt GNU/AGPL, see LICENSE.txt
+ * @author Abhiroop Bhatnagar <bhatnagarabhiroop@gmail.com>
+ */
+class ConfigurationController extends Controller
+{
+	public function __construct(Template $template)
+	{
+		parent::__construct($template);
+	}
+	public function index()
+	{	
+		
+	}
+	public function update()
+	{
+		if (isset($_POST['serviceDiscoveryURL'])) 
+		{
+			ConfigurationList::handleUpdate($_POST);
+			try {
+				$_SESSION['USER']->save();
+				header('Location: '.BASE_URL.'/configuration');
+				exit();
+			}
+			catch (Exception $e) {
+				$_SESSION['errorMessages'][] = $e;
+			}
+		}
+		$this->template->blocks[]=new Block('configPanel/configList.inc');
+	}
+}
+?>
