6301cd5f259a84ad4cae0331daa9aede5b78f1cf
Abhiroop Bhatnagar <bhatnagarabhiroop@gmail.com>
2013-10-01 18:54:04 +0530

    Update SMSController.php

--- libraries/framework/classes/SMSController.php
+++ libraries/framework/classes/SMSController.php
@@ -2,7 +2,7 @@
 /**
  * @copyright 2012 City of Bloomington, Indiana
  * @license http://www.gnu.org/licenses/agpl.txt GNU/AGPL, see LICENSE.txt
- * @author Cliff Ingham <inghamn@bloomington.in.gov>
+ * @author Abhiroop Bhatnagar<bhatnagarabhiroop@gmail.com>
  */
 abstract class SMSController extends Controller
 {
